9e561874ff9a348dc8fc51c5dcd105852081160e
anterlee <anter@nemoplay.co.kr>
2021-03-01 11:44:02 +0900

    Initial Commit

---
+++ LocaleTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LocaleTest
+//
+//  Created by anterlee on 2021/03/01.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
